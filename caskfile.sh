67967caf4762445d447125241d2c27a5f3e49c8c
zendy <zendywongso@gmail.com>
2015-09-16 14:12:12 +1000

    first commit

---
+++ caskfile.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+brew cask install alfred 2> /dev/null
+brew cask install appcleaner 2> /dev/null
+brew cask install box-sync 2> /dev/null
+brew cask install caffeine 2> /dev/null
+brew cask install dropbox 2> /dev/null
+brew cask install fantastical 2> /dev/null
+brew cask install filezilla 2> /dev/null
+brew cask install flux 2> /dev/null
+brew cask install fontprep 2> /dev/null
+brew cask install google-chrome 2> /dev/null
+brew cask install istat-menus 2> /dev/null
+brew cask install iterm2 2> /dev/null
+brew cask install rightfont 2> /dev/null
+brew cask install slack 2> /dev/null
+brew cask install spectacle 2> /dev/null
+brew cask install sublime-text 2> /dev/null
+brew cask install the-unarchiver 2> /dev/null
+brew cask install torbrowser 2> /dev/null
+brew cask install virtualbox 2> /dev/null
+brew cask install vlc 2> /dev/null
+brew cask install vox 2> /dev/null
